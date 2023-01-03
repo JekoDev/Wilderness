dc19fc6bd8b5cb9fd95d83750064cf80ba20105e
JekoDev <52203544+JekoDev@users.noreply.github.com>
2023-01-03 15:14:16 +0100

    Devtools- MapEditor
    Save & Load Included - working (quick and dirty)

--- js/game/map.js
+++ js/game/map.js
@@ -53,39 +53,45 @@ class Map{
 
 				if(this.tiles[x][y] == null){
 	 			
-	 				this.tiles[x][y] = new Tile();
+	 				this.tiles[x][y] = new Tile(0);
 					this.tiles[x][y].hex = hex;
 				
 				}
 
-
-				switch(this.tiles[x][y].hexagon){
+				switch(this.tiles[x][y].type){
 					case 0: //Flat
 						var hex = new PIXI.Sprite(hexagon_flat);
+						this.tiles[x][y].type = 0;
 						break;
 
 					case 1: //Mountain
 						var hex = new PIXI.Sprite(hexagon_mountain);
+						this.tiles[x][y].type = 1;
 						break;
 
 					case 2: //Forest
 						var hex = new PIXI.Sprite(hexagon_forest);
+						this.tiles[x][y].type = 2;
 						break;
 
 					case 3: //Water
 						var hex = new PIXI.Sprite(hexagon_water);
+						this.tiles[x][y].type = 3;
 						break;
 
 					case 4: //End
 						var hex = new PIXI.Sprite(hexagon_end);
+						this.tiles[x][y].type = 4;
 						break;
 
 					case 5: //Start
 						var hex = new PIXI.Sprite(hexagon_start);
+						this.tiles[x][y].type = 5;
 						break;
 
 					case 6: //Mountain_Dead
 						var hex = new PIXI.Sprite(hexagon_mountain_dead);
+						this.tiles[x][y].type = 6;
 						break;
 				}
 
@@ -113,6 +119,8 @@ class Map{
 
 		_wilderness_mapcontainer.interactive = true;
 		_wilderness_mapcontainer.buttonMode = true;
+		_wilderness_mapcontainer.position.x = -20;
+		_wilderness_mapcontainer.position.y = -20;
 
 		_wilderness_mapcontainer
 	        .on('pointerdown', _hexmove_onDragStart)
@@ -140,9 +148,112 @@ class Map{
 		return null;
 	}
 
+	//Get MapData JSON
+	getData(){
+		var ret = [];
+		ret.push(0);
+		for(var y=0; y<this.height; y++){
+			for(var x=0; x<this.width; x++){
+				ret.push(this.tiles[x][y].type);
+			}
+		}
+		return ret;
+	}
+
+
 	//Set MapData Json
 	setData(data){
 		this.tiles = data;
+		for (var i = _wilderness_mapcontainer.children.length - 1; i >= 0; i--) {	_wilderness_mapcontainer.removeChild(_wilderness_mapcontainer.children[i]);};
+		var counter = 1;
+		console.log(data);
+
+		this.tiles = [];
+		if (this.tiles.count == null){
+			for (this.tiles=[]; this.tiles.push([])<this.height;);
+			//randomness function for details goes heeeeere
+			//later share over network
+		}
+
+
+		for(var y=0; y<this.height; y++){
+			for(var x=0; x<this.width; x++){
+				var triggerx = 0;
+				if(x%2 == 1) triggerx = 1;
+
+
+
+				if(this.tiles[x][y] == null){
+	 			
+	 				this.tiles[x][y] = new Tile(0);
+					this.tiles[x][y].hex = hex;
+				
+				}
+
+				this.tiles[x][y].type = data[counter];
+				counter++;
+				
+
+
+				switch(this.tiles[x][y].type){
+					case 0: //Flat
+						var hex = new PIXI.Sprite(hexagon_flat);
+						this.tiles[x][y].type = 0;
+						break;
+
+					case 1: //Mountain
+						var hex = new PIXI.Sprite(hexagon_mountain);
+						this.tiles[x][y].type = 1;
+						break;
+
+					case 2: //Forest
+						var hex = new PIXI.Sprite(hexagon_forest);
+						this.tiles[x][y].type = 2;
+						break;
+
+					case 3: //Water
+						var hex = new PIXI.Sprite(hexagon_water);
+						this.tiles[x][y].type = 3;
+						break;
+
+					case 4: //End
+						var hex = new PIXI.Sprite(hexagon_end);
+						this.tiles[x][y].type = 4;
+						break;
+
+					case 5: //Start
+						var hex = new PIXI.Sprite(hexagon_start);
+						this.tiles[x][y].type = 5;
+						break;
+
+					case 6: //Mountain_Dead
+						var hex = new PIXI.Sprite(hexagon_mountain_dead);
+						this.tiles[x][y].type = 6;
+						break;
+				}
+
+
+				if (triggerx == 0){
+					hex.transform.position.y = y*90 - 45;
+				}else{
+					hex.transform.position.y = y*90;
+				}
+
+				hex.transform.position.x = x*80;
+				hex.transform.scale.x = 0.15;
+				hex.transform.scale.y = 0.15;
+				//hex.alpha = Math.random();
+				_wilderness_mapcontainer.addChild(hex);
+
+
+				hex.interactive = true;
+				hex.buttonMode = true;
+				hex.on("pointerup", _hexclick_onDragStart);
+				
+				//Generation goes here
+			}
+		}
+
 	}
 }
 	
@@ -154,65 +265,11 @@ class Map{
 				if (wilderness_map.tiles[x][y].hex == this){
 		//			wilderness_player.setPos(x,y);
 					var hextype = parseInt($(".devtool_bttn.mla.active").first().attr("data"));
-					console.log(hextype);
 					_wilderness_mapcontainer.removeChild(wilderness_map.tiles[x][y].hex);
-					switch(hextype){
-						case 0: //Flat
-							var hex = new PIXI.Sprite(hexagon_flat);
-							break;
-
-						case 1: //Mountain
-							var hex = new PIXI.Sprite(hexagon_mountain);
-							break;
-
-						case 2: //Forest
-							var hex = new PIXI.Sprite(hexagon_forest);
-							break;
-
-						case 3: //Water
-							var hex = new PIXI.Sprite(hexagon_water);
-							break;
-
-						case 4: //End
-							var hex = new PIXI.Sprite(hexagon_end);
-							break;
-
-						case 5: //Start
-							var hex = new PIXI.Sprite(hexagon_start);
-							break;
-
-						case 6: //Mountain_Dead
-							var hex = new PIXI.Sprite(hexagon_mountain_dead);
-							break;
-					}
-
-					wilderness_map.tiles[x][y].hex = hex;
-
-
-					var triggerx = 0;
-					if((x-1)%2 == 1) triggerx = 1;
-
-					if (triggerx == 0){
-						hex.transform.position.y = y*90 - 45;
-					}else{
-						hex.transform.position.y = y*90;
-					}
-
-					hex.transform.position.x = (x-1)*80;
-					hex.transform.scale.x = 0.15;
-					hex.transform.scale.y = 0.15;
-					//hex.alpha = Math.random();
-					_wilderness_mapcontainer.addChild(hex);
-
-
-					hex.interactive = true;
-					hex.buttonMode = true;
-					hex.on("pointerup", _hexclick_onDragStart);
-
-
-					//$("#programming_debug").val(JSON.stringify(wilderness_map.tiles));
-					//Cycling Shit Error
-
+					x=x-1;
+					wilderness_map.tiles[x][y].type = hextype;
+					$("#programming_debug").val(JSON.stringify(wilderness_map.getData()));
+					wilderness_map.setData(wilderness_map.getData());
 					return;
 				}
 			}
