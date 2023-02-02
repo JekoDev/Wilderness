04485fb29c985493ca9603fb5701550b3240f94e
JekoDev <52203544+JekoDev@users.noreply.github.com>
2023-02-02 12:02:02 +0100

    preperation network
    just first things

--- js/game/network.js
+++ js/game/network.js
@@ -3,7 +3,42 @@
 =============================================================== */
 
 class Network{
-	//nodejs shizzle
+
+	mode = 0;
+	//0=Client | 1=Server
+
+	rooms = [];
+	ip = "127.0.0.1";
+	port = 80;
+
+	//Functions ===============================================
+
+	Network(){
+		//constructor
+	}
+
+
+	createRoom(){
+
+	}
+
+	refreshRooms(){
+
+	}
+
+	connect(ip, port){
+
+	}
+
+	exchange(data){
+		//exchange data
+	}
+
+	_thread_network(){
+
+	}
 }
+
+
 
 //============================================================= EOF
